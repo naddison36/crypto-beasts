d1f810c9961a8513a468b442883cd1c0c2dea166
Baxter Addison <baxter@Baxters-MacBook-Pro.local>
2019-04-07 15:00:04 +1000

    more cards

--- scratch/extensions/cryptoBeasts/cards.js
+++ scratch/extensions/cryptoBeasts/cards.js
@@ -28,8 +28,8 @@ module.exports =
                 health:375
             },
             player: {
-                attack: 50,
-                specialAttack: 50,
+                attack: 10,
+                specialAttack: 10,
             },
             mana: 10,
         },
@@ -150,12 +150,16 @@ module.exports =
         ability: {
             name: 'Step on Shapes',
             opponent: {
+                defence: 1000000000,
+                health: 50,
+
 
             },
             player: {
+                defence: 100,
 
             },
-            mana: 0,
+            mana: 8,
         },
         health: 950,
         defence: 400,
@@ -169,29 +173,39 @@ module.exports =
         ability: {
             name: 'I\'m throwing stuff at you',
             opponent: {
-            },
-            player: {
 
             },
-            mana: 0,
+            player: {
+                mana: 3,
+                specialAttack: 10,
+                attack: 10,
+                health: 100,
+                defence: 100,
+            },
+            mana: 8,
         },
-        health: 0,
-        defence: 0,
-        mana: 0,
-        speed: 0,
-        attack: 0,
-        specialAttack: 0,
+        health: 990,
+        defence: 390,
+        mana: 5,
+        speed: 60,
+        attack: 90,
+        specialAttack: 150,
     },
     {
         name: 'alien UFO',
         ability: {
             name: 'abduction',
             opponent: {
+                mana: 3,
+                attack: 10,
+                specialAttack: 10,
+                defence: 100,
+                health: 100,
             },
             player: {
 
             },
-            mana: 0,
+            mana: 9,
         },
         health: 910,
         defence: 350,
@@ -205,11 +219,13 @@ module.exports =
         ability: {
             name: 'water (thats all)',
             opponent: {
+                defence: 100000000,
+                health: 175
             },
             player: {
 
             },
-            mana: 0,
+            mana: 9,
         },
         health: 1000,
         defence: 390,
@@ -223,11 +239,12 @@ module.exports =
         ability: {
             name: 'Volcano strick',
             opponent: {
+                Health: 400,
             },
             player: {
 
             },
-            mana: 0,
+            mana: 9,
         },
         health: 990,
         defence: 300,
@@ -241,11 +258,13 @@ module.exports =
         ability: {
             name: 'Give em da horn',
             opponent: {
+                defence: 200,
+                health: 250,
             },
             player: {
 
             },
-            mana: 0,
+            mana: 8,
         },
         health: 920,
         defence: 900,
@@ -257,13 +276,14 @@ module.exports =
     {
         name: 'spy pig ionk ionk',
         ability: {
-            name: '??',
+            name: 'Bacon power',
             opponent: {
+                health: 400,
             },
             player: {
 
             },
-            mana: 0,
+            mana: 8,
         },
         health: 950,
         defence: 350,
@@ -271,5 +291,499 @@ module.exports =
         speed: 100,
         attack: 60,
         specialAttack: 110,
+    },
+    {
+        name: 'Gress',
+        ability: {
+            name: 'Healing grouth',
+            opponent: {
+                
+            },
+            player: {
+                health: 400,
+        
+            },
+            mana: 9,
+        },
+        health: 950,
+        defence: 210,
+        mana: 7,
+        speed: 100,
+        attack: 60,
+        specialAttack: 110,
+    },
+    {
+        name: 'blazix',
+        ability: {
+            name: 'firestorm',
+            opponent: {
+                health: 401,
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 930,
+        defence: 240,
+        mana: 8,
+        speed: 120,
+        attack: 75,
+        specialAttack: 130,
+    },
+    {
+        name: 'rat face',
+        ability: {
+            name: 'im in your face',
+            opponent: {
+                health: 402,
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 990,
+        defence: 390,
+        mana: 6,
+        speed: 60,
+        attack: 60,
+        specialAttack: 100,
+    },
+    {
+        name: 'spy trunks',
+        ability: {
+            name: 'break da shell',
+            opponent: {
+                defence:100000, 
+            },
+            player: {
+                defence: 200,
+        
+            },
+            mana: 9,
+        },
+        health: 990,
+        defence: 400,
+        mana: 6,
+        speed: 55,
+        attack: 60,
+        specialAttack: 110,
+    },
+    {
+        name: 'im a cat',
+        ability: {
+            name: 'cat scratch (no pun intended)',
+            opponent: {
+                health: 200,
+                defence: 200,
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 970,
+        defence: 350,
+        mana: 6,
+        speed: 120,
+        attack: 70,
+        specialAttack: 120,
+    },
+    {
+        name: 'bamboo boy',
+        ability: {
+            name: 'bamboo staff',
+            opponent: {
+                defence: 100000,
+            },
+            player: {
+                defence: 230,
+        
+            },
+            mana: 9,
+        },
+        health: 960,
+        defence: 360,
+        mana: 6,
+        speed: 90,
+        attack: 70,
+        specialAttack: 120,
+    },
+    {
+        name: 'snow face',
+        ability: {
+            name: 'hypothermia',
+            opponent: {
+                mana: 3,
+                attack: 10,
+                specialAttack: 10,
+                defence: 100,
+                health: 100,
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 950,
+        defence: 250,
+        mana: 7,
+        speed: 100,
+        attack: 60,
+        specialAttack: 110,
+    },
+    {
+        name: 'spy plush',
+        ability: {
+            name: 'plushy cuteness',
+            opponent: {
+                
+                
+                
+                defence: 1000000,
+                
+            },
+            player: {
+        
+            },
+            mana: 8,
+        },
+        health: 900,
+        defence: 300,
+        mana: 7,
+        speed: 100,
+        attack: 50,
+        specialAttack: 90,
+    },
+    {
+        name: 'Koala (thats all)',
+        ability: {
+            name: 'aussie sleepers',
+            opponent: {
+                mana: 3,
+                attack: 10,
+                specialAttack: 10,
+                defence: 100,
+                health: 100,
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 990,
+        defence: 390,
+        mana: 5,
+        speed: 80,
+        attack: 50,
+        specialAttack: 90,
+    },
+    {
+        name: 'darkmato',
+        ability: {
+            name: 'sauce slash',
+            opponent: {
+                
+                health: 403,
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 990,
+        defence: 250,
+        mana: 6,
+        speed: 70,
+        attack: 70,
+        specialAttack: 120,
+    },
+    {
+        name: 'lightmato',
+        ability: {
+            name: 'get saucy with it',
+            opponent: {
+   
+                health: 403,
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 910,
+        defence: 210,
+        mana: 6,
+        speed: 130,
+        attack: 60,
+        specialAttack: 100,
+    },
+    {
+        name: ' spy bear',
+        ability: {
+            name: 'bear claw',
+            opponent: {
+  
+                health: 399,
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 900,
+        defence: 300,
+        mana: 6,
+        speed: 90,
+        attack: 50,
+        specialAttack: 90,
+    },
+    {
+        name: 'spy trash',
+        ability: {
+            name: 'trash alert!!!',
+            opponent: {
+                mana: 5,
+               
+                defence: 1000000,
+
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 930,
+        defence: 200,
+        mana: 6,
+        speed: 100,
+        attack: 80,
+        specialAttack: 140,
+    },
+    {
+        name: 'slither',
+        ability: {
+            name: 'venom strick',
+            opponent: {
+                      
+                health: 404,
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 940,
+        defence: 360,
+        mana: 5,
+        speed: 130,
+        attack: 70,
+        specialAttack: 120,
+    },
+    {
+        name: 'medusa',
+        ability: {
+            name: 'stone glare',
+            opponent: {
+                mana: 100000,
+               
+              
+                defence: 10000,
+            
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 850,
+        defence: 320,
+        mana: 8,
+        speed: 120,
+        attack: 70,
+        specialAttack: 120,
+    },
+    {
+        name: 'puffafish',
+        ability: {
+            name: 'puff me up boy',
+            opponent: {
+      
+                health: 100,
+            },
+            player: {
+                defence:300 
+        
+            },
+            mana: 9,
+        },
+        health: 900,
+        defence: 400,
+        mana: 6,
+        speed: 70,
+        attack: 70,
+        specialAttack: 120,
+    },
+    {
+        name: 'tiger',
+        ability: {
+            name: 'im the eye of the tiger',
+            opponent: {
+               
+                health: 398,
+            },
+            player: {
+        
+            },
+            mana: 8,
+        },
+        health: 960,
+        defence: 370,
+        mana: 6,
+        speed: 110,
+        attack: 70,
+        specialAttack: 120,
+    },
+    {
+        name: 'horse',
+        ability: {
+            name: 'dump of rider',
+            opponent: {
+               
+                defence: 200,
+                health: 200,
+            },
+            player: {
+        
+            },
+            mana: 8,
+        },
+        health: 900,
+        defence: 250,
+        mana: 6,
+        speed: 120,
+        attack: 80,
+        specialAttack: 140,
+    },
+    {
+        name: 'unicorn',
+        ability: {
+            name: 'HORN! of the unicorn' ,
+            opponent: {
+                  
+               
+            },
+            player: {
+                attack: 10,
+                specialAttack: 10,
+        
+            },
+            mana: 8,
+        },
+        health: 950,
+        defence: 370,
+        mana: 9,
+        speed: 110,
+        attack: 60,
+        specialAttack: 100,
+    }, {
+        name: 'sun lion ',
+        ability: {
+            name: 'ROAR!!!',
+            opponent: {
+              
+                health: 405,
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 990,
+        defence: 390,
+        mana: 6,
+        speed: 110,
+        attack: 80,
+        specialAttack: 140,
+    }, {
+        name: 'shark',
+        ability: {
+            name: 'shark nado',
+            opponent: {
+              
+                health: 406,
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 990,
+        defence: 390,
+        mana: 6,
+        speed: 110,
+        attack: 70,
+        specialAttack: 120,
+    }, {
+        name: 'monster reptile',
+        ability: {
+            name: 'im a monster inside (and out)',
+            opponent: {
+             
+                health: 407,
+            },
+            player: {
+        
+            },
+            mana: 10,
+        },
+        health: 990,
+        defence: 400,
+        mana: 8,
+        speed: 110,
+        attack: 80,
+        specialAttack: 140,
+    }, {
+        name: 'scarecrow',
+        ability: {
+            name: 'im scary',
+            opponent: {
+              
+                defence: 10000,
+                health: 100,
+           
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 950,
+        defence: 400,
+        mana: 7,
+        speed:60,
+        attack: 60,
+        specialAttack: 100,
+    }, {
+        name: 'octopus',
+        ability: {
+            name: 'lots of legs',
+            opponent: {
+                
+                defence: 10000,
+                health: 120,
+            },
+            player: {
+        
+            },
+            mana: 9,
+        },
+        health: 930,
+        defence: 330,
+        mana: 5,
+        speed:90,
+        attack: 50,
+        specialAttack: 90,
     },
 ]
