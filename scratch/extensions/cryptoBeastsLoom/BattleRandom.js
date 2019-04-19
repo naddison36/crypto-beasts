318e4f7897f7735fc7e5865faaa58c13ec83d8cf
Nicholas Addison <>
2019-04-19 14:15:20 +1000

    Change async/await to normal Promises

--- scratch/extensions/cryptoBeastsLoom/BattleRandom.js
+++ scratch/extensions/cryptoBeastsLoom/BattleRandom.js
@@ -49,93 +49,124 @@ class Battle {
         log.debug(`Battle contract address is ${this.contract.options.address}`)
     }
 
-    async deploy(opponentPlayer) {
-        log.debug(`About to deploy battle contract with player 1 ${this.myPlayer}, player 2 ${opponentPlayer} and cards contract address ${CardsContract.networks[network].address}`)
+    deploy(opponentPlayer) {
 
-        this.contract = await this.contract.deploy({
-            data: BattleContract.bytecode,
-            arguments: [this.myPlayer, opponentPlayer, CardsContract.networks[network].address],
-        }).send({
-            from: this.myPlayer,
+        return new Promise((resolve, reject) => {
+            log.debug(`About to deploy battle contract with player 1 ${this.myPlayer}, player 2 ${opponentPlayer} and cards contract address ${CardsContract.networks[network].address}`)
+
+            this.contract.deploy({
+                data: BattleContract.bytecode,
+                arguments: [this.myPlayer, opponentPlayer, CardsContract.networks[network].address],
+            })
+            .send({
+                from: this.myPlayer,
+            })
+            .catch(err => {
+                reject(new Error(`Failed to deploy Battle contract. ${err.message}`))
+            })
+            .then(contract => {
+                this.contract = contract
+
+                log.info(`Deployed Battle contract address ${this.contract.options.address}`)
+    
+                resolve(this.contract.options.address)
+            })
         })
+    }
+
+    getCard(cardId) {
+        return this.contract.methods.cards(cardId).call()
+    }
+
+    getPlayerDeck(player) {
+
+        return new Promise((resolve, reject) => {
+
+            this.contract.methods.getPlayerDeck(player).call()
+            .then(returnedDeck => {
+                if (returnedDeck.playerCards.length === 0) {
+                    return reject(Error(`Failed to get player deck for ${player}. They probably have not picked their cards yet`))
+                }
+    
+                function parsePlayerCard(returnedPlayerCard) {
+                    return {
+                        cardId: parseInt(returnedPlayerCard[0]),
+                        health: parseInt(returnedPlayerCard[1]),
+                        defence: parseInt(returnedPlayerCard[2]),
+                        mana: parseInt(returnedPlayerCard[3]),
+                        attack: parseInt(returnedPlayerCard[4]),
+                        specialAttack: parseInt(returnedPlayerCard[5]),
+                    }
+                }
+    
+                resolve({
+                    playerCards: [
+                        parsePlayerCard(returnedDeck.playerCards[0]),
+                        parsePlayerCard(returnedDeck.playerCards[1]),
+                        parsePlayerCard(returnedDeck.playerCards[2]),
+                    ],
+                    currentCard: parseInt(returnedDeck[1])
+                })
+            })
+            .catch(err => {
+                reject(`Failed to get player deck for ${player}. Error: ${err.message}`)
+            })
+        })
+    }
+
+    getPlayer1() {
+        return this.contract.methods.player1().call()
+    }
+
+    getPlayer2() {
+        return this.contract.methods.player2().call()
+    }
+
+    getWinningPlayer() {
+        return this.contract.methods.winningPlayer().call()
+    }
+
+    pickPayerCards(desiredCards) {
+
+        return new Promise((resolve, reject) => {
+            log.debug(`About to call pickPayerCards with arg ${JSON.stringify(desiredCards)}, signed by ${this.myPlayer}`)
+
+            this.contract.methods
+            .pickPayerCards(desiredCards)
+            .send({from: this.myPlayer})
+            .then(tx => {
+                const pickedCardsStr = tx.events.PickPayerCards.returnValues.pickedCards
+                const pickedCardsInt = pickedCardsStr.map(x => parseInt(x))
         
-        log.info(`Deployed Battle contract address ${this.contract.options.address}`)
-
-        return this.contract.options.address
+                log.debug(`Player ${this.myPlayer} picked cards ${JSON.stringify(pickedCardsInt)}`)
+        
+                resolve(pickedCardsInt)
+            })
+            .catch(err => {
+                reject(`Failed to pick player cards for ${this.myPlayer}`)
+            })
+        })
     }
 
-    async getCard(cardId) {
-        return await this.contract.methods.cards(cardId).call()
-    }
+    turn(move) {
 
-    async getPlayerDeck(player) {
-        const returnedDeck = await this.contract.methods.getPlayerDeck(player).call()
+        return new Promise((resolve, reject) => {
+            log.debug(`About to move ${move}, signed by ${this.myPlayer}`)
 
-        if (returnedDeck.playerCards.length === 0) {
-            throw Error(`Failed to get player deck for ${player}. They probably have not picked their cards yet`)
-        }
+            this.contract.methods
+            .turn(move)
+            .send({from: this.myPlayer})
+            .then(tx => {
+                const turnEvent = tx.events.PickPayerCards.returnValues.Turn
 
-        function parsePlayerCard(returnedPlayerCard) {
-            return {
-                cardId: parseInt(returnedPlayerCard[0]),
-                health: parseInt(returnedPlayerCard[1]),
-                defence: parseInt(returnedPlayerCard[2]),
-                mana: parseInt(returnedPlayerCard[3]),
-                attack: parseInt(returnedPlayerCard[4]),
-                specialAttack: parseInt(returnedPlayerCard[5]),
-            }
-        }
-
-        return {
-            playerCards: [
-                parsePlayerCard(returnedDeck.playerCards[0]),
-                parsePlayerCard(returnedDeck.playerCards[1]),
-                parsePlayerCard(returnedDeck.playerCards[2]),
-            ],
-            currentCard: parseInt(returnedDeck[1])
-        }
-    }
-
-    async getPlayer1() {
-        return await this.contract.methods.player1().call()
-    }
-
-    async getPlayer2() {
-        return await this.contract.methods.player2().call()
-    }
-
-    async getWinningPlayer() {
-        return await this.contract.methods.winningPlayer().call()
-    }
-
-    async pickPayerCards(desiredCards) {
-
-        log.debug(`About to call pickPayerCards with arg ${JSON.stringify(desiredCards)}, signed by ${this.myPlayer}`)
-
-        const tx = await this.contract.methods
-        .pickPayerCards(desiredCards)
-        .send({from: this.myPlayer})
-
-        const pickedCardsStr = tx.events.PickPayerCards.returnValues.pickedCards
-        const pickedCardsInt = pickedCardsStr.map(x => parseInt(x))
-
-        log.debug(`Player ${this.myPlayer} picked cards ${JSON.stringify(pickedCardsInt)}`)
-
-        return pickedCardsInt
-    }
-
-    async turn(move) {
-        log.debug(`About to move ${move}, signed by ${this.myPlayer}`)
-
-        const tx = await this.contract.methods
-        .turn(move)
-        .send({from: this.myPlayer})
-
-        const turnEvent = tx.events.PickPayerCards.returnValues.Turn
-
-        log.info(`Got turn event for player ${this.myPlayer}  ${JSON.stringify(turnEvent)}`)
-
-        return pickedCardsInt
+                log.info(`Got turn event for player ${this.myPlayer}  ${JSON.stringify(turnEvent)}`)
+    
+                resolve(pickedCardsInt)
+            })
+            .catch(err => {
+                reject(`Failed to move ${move} for player ${this.myPlayer}`)
+            })
+        })
     }
 }
 
